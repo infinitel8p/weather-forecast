d0d77f54e6d2f5d366acac92678ff183ed84684d
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2022-07-15 16:01:02 +0200

    cleared code

--- backend/test/api_fetch.py
+++ backend/test/api_fetch.py
@@ -1,11 +1,8 @@
 import requests
 import json
 
-test_call = requests.get('http://127.0.0.1:8000/')
-test_call_parsed = json.loads(test_call.text)
-print(test_call_parsed)
-
 test_call = requests.get('http://127.0.0.1:8000/test')
 test_call_parsed = json.loads(test_call.text)
 print(test_call_parsed)
-print(test_call_parsed["last_updated"])
+for key in test_call_parsed:
+    print(key, ':', test_call_parsed[key])
