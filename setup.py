88bd10012eaafd652e11aa9cfd0a207b6bf0091a
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2022-07-17 12:06:01 +0200

    added threading

--- setup.py
+++ setup.py
@@ -1,8 +1,30 @@
 import os
+import threading
 import webbrowser
 
-os.system("pip install -r requirements.txt")
-webbrowser.open("http://127.0.0.1:8000", new=0, autoraise=True)
-webbrowser.open(os.path.dirname(__file__) +
-                "/frontend/test/index.html", new=0, autoraise=True)
-os.system("python -m uvicorn backend.main:app --reload")
+
+def install_requirements():
+    os.system("pip install -r requirements.txt")
+
+
+def open_browser():
+    webbrowser.open("http://127.0.0.1:8000", new=0, autoraise=True)
+
+    webbrowser.open(os.path.dirname(__file__) +
+                    "/frontend/test/index.html", new=0, autoraise=True)
+
+
+if __name__ == "__main__":
+    # creating thread
+    t1 = threading.Thread(target=install_requirements)
+    t2 = threading.Thread(target=open_browser)
+
+    # starting thread 1 & 2
+    t1.start()
+    t2.start()
+
+    # wait until thread 1 & 2 is completely executed
+    t1.join()
+    t2.join()
+
+    os.system("python -m uvicorn backend.main:app --reload")
