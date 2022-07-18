b8665d9a80ae82f373987e230bca72748886d132
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2022-07-18 07:41:21 +0200

    (re-) moved old files

--- backend/test/api_fetch.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-test_call = requests.get('http://127.0.0.1:8000/test')
-test_call_parsed = json.loads(test_call.text)
-print(test_call_parsed)
-for key in test_call_parsed:
-    print(key, ':', test_call_parsed[key])
