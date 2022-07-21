6bdf4d80baaa1e456f24beb30d56118eae328519
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2022-07-21 08:00:47 +0200

    changed code to dynamically edit objects on page

--- frontend/script.js
+++ frontend/script.js
@@ -78,35 +78,20 @@ async function fetchForecastWeatherWeek() {
 		// convert var data to json object
 		data = JSON.parse(data);
 		console.log(data);
-		document.getElementById('day_pred_1').textContent = data.day1.day;
-		document.getElementById('temp_min_1').textContent = data.day1.temp;
-		document.getElementById('temp_max_1').textContent = data.day1.condition;
-		document.getElementById('icon_1').outerHTML = data.day1.svg;
-
-		document.getElementById('day_pred_2').textContent = data.day2.day;
-		document.getElementById('temp_min_2').textContent = data.day2.temp;
-		document.getElementById('temp_max_2').textContent = data.day2.condition;
-		document.getElementById('icon_2').outerHTML = data.day2.svg;
-
-		document.getElementById('day_pred_3').textContent = data.day3.day;
-		document.getElementById('temp_min_3').textContent = data.day3.temp;
-		document.getElementById('temp_max_3').textContent = data.day3.condition;
-		document.getElementById('icon_3').outerHTML = data.day3.svg;
-
-		document.getElementById('day_pred_4').textContent = data.day4.day;
-		document.getElementById('temp_min_4').textContent = data.day4.temp;
-		document.getElementById('temp_max_4').textContent = data.day4.condition;
-		document.getElementById('icon_4').outerHTML = data.day4.svg;
-
-		document.getElementById('day_pred_5').textContent = data.day5.day;
-		document.getElementById('temp_min_5').textContent = data.day5.temp;
-		document.getElementById('temp_max_5').textContent = data.day5.condition;
-		document.getElementById('icon_5').outerHTML = data.day5.svg;
+		for (let i = 1; i < 6; i++) {
+			document.getElementById('day_pred_' + i).textContent =
+				data[`day${i}`].day;
+			document.getElementById('temp_min_' + i).textContent =
+				data[`day${i}`].temp;
+			document.getElementById('temp_max_' + i).textContent =
+				data[`day${i}`].condition;
+			document.getElementById('icon_' + i).outerHTML = data[`day${i}`].svg;
+		}
 
 		var svg = document.querySelectorAll('svg');
 		for (const element of svg) {
-			element.setAttribute('height', '75px');
-			element.setAttribute('width', '75px');
+			element.setAttribute('height', '150px');
+			element.setAttribute('width', '150px');
 		}
 	}
 }
@@ -122,77 +107,23 @@ async function fetchForecastWeatherDay() {
 		// convert var data to json object
 		data = JSON.parse(data);
 		console.log(data);
-		document.getElementById('time_1').textContent = data.forecast1.time;
-		document.getElementById('icon_hrs_1').src = data.forecast1.icon;
-		document.getElementById('weather_description_1').textContent =
-			data.forecast1.description;
-		document.getElementById('temp_1').textContent = data.forecast1.temp;
-		document.getElementById('humidity_1').textContent = data.forecast1.humidity;
-		document.getElementById('rain_1').textContent = data.forecast1.rain;
-		document.getElementById('clouds_1').textContent = data.forecast1.clouds;
-
-		document.getElementById('time_2').textContent = data.forecast2.time;
-		document.getElementById('icon_hrs_2').src = data.forecast2.icon;
-		document.getElementById('weather_description_2').textContent =
-			data.forecast2.description;
-		document.getElementById('temp_2').textContent = data.forecast2.temp;
-		document.getElementById('humidity_2').textContent = data.forecast2.humidity;
-		document.getElementById('rain_2').textContent = data.forecast2.rain;
-		document.getElementById('clouds_2').textContent = data.forecast2.clouds;
-
-		document.getElementById('time_3').textContent = data.forecast3.time;
-		document.getElementById('icon_hrs_3').src = data.forecast3.icon;
-		document.getElementById('weather_description_3').textContent =
-			data.forecast3.description;
-		document.getElementById('temp_3').textContent = data.forecast3.temp;
-		document.getElementById('humidity_3').textContent = data.forecast3.humidity;
-		document.getElementById('rain_3').textContent = data.forecast3.rain;
-		document.getElementById('clouds_3').textContent = data.forecast3.clouds;
-
-		document.getElementById('time_4').textContent = data.forecast4.time;
-		document.getElementById('icon_hrs_4').src = data.forecast4.icon;
-		document.getElementById('weather_description_4').textContent =
-			data.forecast4.description;
-		document.getElementById('temp_4').textContent = data.forecast4.temp;
-		document.getElementById('humidity_4').textContent = data.forecast4.humidity;
-		document.getElementById('rain_4').textContent = data.forecast4.rain;
-		document.getElementById('clouds_4').textContent = data.forecast4.clouds;
-
-		document.getElementById('time_5').textContent = data.forecast5.time;
-		document.getElementById('icon_hrs_5').src = data.forecast5.icon;
-		document.getElementById('weather_description_5').textContent =
-			data.forecast5.description;
-		document.getElementById('temp_5').textContent = data.forecast5.temp;
-		document.getElementById('humidity_5').textContent = data.forecast5.humidity;
-		document.getElementById('rain_5').textContent = data.forecast5.rain;
-		document.getElementById('clouds_5').textContent = data.forecast5.clouds;
-
-		document.getElementById('time_6').textContent = data.forecast6.time;
-		document.getElementById('icon_hrs_6').src = data.forecast6.icon;
-		document.getElementById('weather_description_6').textContent =
-			data.forecast6.description;
-		document.getElementById('temp_6').textContent = data.forecast6.temp;
-		document.getElementById('humidity_6').textContent = data.forecast6.humidity;
-		document.getElementById('rain_6').textContent = data.forecast6.rain;
-		document.getElementById('clouds_6').textContent = data.forecast6.clouds;
-
-		document.getElementById('time_7').textContent = data.forecast7.time;
-		document.getElementById('icon_hrs_7').src = data.forecast7.icon;
-		document.getElementById('weather_description_7').textContent =
-			data.forecast7.description;
-		document.getElementById('temp_7').textContent = data.forecast7.temp;
-		document.getElementById('humidity_7').textContent = data.forecast7.humidity;
-		document.getElementById('rain_7').textContent = data.forecast7.rain;
-		document.getElementById('clouds_7').textContent = data.forecast7.clouds;
-
-		document.getElementById('time_8').textContent = data.forecast8.time;
-		document.getElementById('icon_hrs_8').src = data.forecast8.icon;
-		document.getElementById('weather_description_8').textContent =
-			data.forecast8.description;
-		document.getElementById('temp_8').textContent = data.forecast8.temp;
-		document.getElementById('humidity_8').textContent = data.forecast8.humidity;
-		document.getElementById('rain_8').textContent = data.forecast8.rain;
-		document.getElementById('clouds_8').textContent = data.forecast8.clouds;
+		for (let i = 1; i < 9; i++) {
+			document.getElementById('time_' + i).textContent =
+				data[`forecast${i}`].time;
+			document.getElementById('icon_hrs_' + i).src = data[`forecast${i}`].icon;
+			document.getElementById('icon_hrs_' + i).setAttribute('height', '125px');
+			document.getElementById('icon_hrs_' + i).setAttribute('width', '125px');
+			document.getElementById('weather_description_' + i).textContent =
+				data[`forecast${i}`].description;
+			document.getElementById('temp_' + i).outerHTML =
+				'temp:' + data[`forecast${i}`].temp;
+			document.getElementById('humidity_' + i).textContent =
+				'humi:' + data[`forecast${i}`].humidity;
+			document.getElementById('rain_' + i).textContent =
+				'rain:' + data[`forecast${i}`].rain;
+			document.getElementById('clouds_' + i).outerHTML =
+				'cloud:' + data[`forecast${i}`].clouds;
+		}
 	}
 }
 
