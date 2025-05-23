c5768a70365d26fb809edaee07c252e1be375ba5
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2025-05-24 01:11:41 +0200

    feat: add city input to location setup and update API to handle city data

--- src/pages/api/set-location.ts
+++ src/pages/api/set-location.ts
@@ -2,14 +2,26 @@
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ request, cookies }) => {
-    const { lat, lon } = await request.json();
+    const { lat, lon, other } = await request.json();
+    const env = import.meta.env;
 
-    if (!lat || !lon) {
-        return new Response("Bad Request", { status: 400 });
+    if (lat && lon) {
+        cookies.set("lat", lat.toString(), { path: "/" });
+        cookies.set("lon", lon.toString(), { path: "/" });
     }
+    if (other) {
+        let request = await fetch(
+            `https://api.openweathermap.org/data/2.5/find?q=${other}&appid=${env.OPENWEATHERMAP_API_KEY}&units=metric`
+        );
+        const geoCode = await request.json();
 
-    cookies.set("lat", lat.toString(), { path: "/" });
-    cookies.set("lon", lon.toString(), { path: "/" });
+        cookies.set("lat", geoCode.list[0].coord.lat.toString(), { path: "/" });
+        cookies.set("lon", geoCode.list[0].coord.lon.toString(), { path: "/" });
+        cookies.set("city", `${geoCode.list[0].name}, ${geoCode.list[0].sys.country}`, { path: "/" });
+    }
+    else {
+        return new Response("No location provided", { status: 400 });
+    }
 
     return new Response("OK");
 };
